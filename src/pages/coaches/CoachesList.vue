3e50b61e97373786e19988b4ddf933e2268ec32b
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-02-10 01:31:23 -0500

    Coaches list & items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,7 +7,13 @@
     </div>
     <ul v-if="hasCoaches">
       <li v-for="coach in coaches" :key="coach.id">
-        {{ coach.firstName }}
+        <coach-item
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        />
       </li>
     </ul>
     <h3 v-else>No coaches found.</h3>
@@ -16,10 +22,26 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import CoachItem from '../../components/coaches/CoachItem.vue';
 
 export default {
+  components: { CoachItem },
+
   computed: {
     ...mapGetters('coaches', ['coaches', 'hasCoaches']),
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
