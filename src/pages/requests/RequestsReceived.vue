c26ffb52b82eb107156f3e18e37399e20fb73b56
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-02-11 12:34:01 -0500

    Add Requests received module

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,48 @@
-<template>REQUESTS</template>
+<template>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Recieved</h2>
+      </header>
+      <div v-if="hasRequests">
+        <ul v-for="request in requests" :key="request.id">
+          <request-item
+            :message="request.message"
+            :userEmail="request.userEmail"
+          />
+        </ul>
+      </div>
+      <h3 v-else>You haven't received any requests!</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import RequestItem from './RequestItem.vue';
+
+export default {
+  components: { RequestItem },
+  computed: {
+    ...mapGetters('requests', ['requests', 'hasRequests']),
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
